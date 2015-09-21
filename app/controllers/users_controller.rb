6876fbb96b0021c422a15742e33cc0b897e40f07
Margarita <margarita.korkina@mail.ru>
2015-09-21 21:37:42 +0300

    try

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,8 @@ class UsersController < ApplicationController
 #query 3
     @sorted_2 = sort.sort_by { |k| k["name"] }
 #query 4
-    @exp = @user.lists.where(title: LIKE 'N%')
+    @exp = select * from @user.lists where title  similar to '%N%'
+    #(title: LIKE 'N%')
 #query 5
      # @exp_lists_a = @user.lists.where(title: '.+a.+')
 #query 6
