45385894cca07115da5dfe6de2f4fd7727edc4ac
Margarita <margarita.korkina@mail.ru>
2015-09-27 22:48:30 +0300

    03

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,8 @@ class UsersController < ApplicationController
 #query 3
     @sorted_2 = sort.sort_by { |k| k["name"] }
 #query 4
-@exp = @user.lists.find_by_sql("SELECT title FROM lists WHERE (lists.title ~* '^N' )").tasks.select(:name)
+@exp = @user.lists.find_by_sql("SELECT title FROM lists WHERE (lists.title ~* '^N' )")
+@exp.tasks.select(:name)
 #query 5
      # @exp_lists_a = @user.lists.where('title REGEXP ".+a.+"')
 #query 6
